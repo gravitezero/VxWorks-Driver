a043c67772320d01664ab64b967fdd62cf0e0f25
Etienne Brodu <etienne.brodu@insa-lyon.fr>
2010-11-03 21:39:33 +0100

    commit final

--- PEDriver/driver.h
+++ PEDriver/driver.h
@@ -8,34 +8,51 @@
 #include <time.h>
 
 
+/* La taille maximale des données d'un message (\0 compris)*/
+#define MAX_DATA_SIZE 64
+/* La taille maximale d'un nom de capteur (chemin complet, et \0 compris) */
+#define NAME_SIZE 32
 
-#define MAX_MSG_SIZE 64
-
+/**
+ * L'état d'un capteur : il peut être fermé, ouvert, ou pas encore
+ * crée.
+ */
 typedef enum
 {
-	closed,
-	opened,
-	notcreated
+	closed,   //!< closed Le capteur est fermé.
+	opened,   //!< opened Le capteur est ouvert.
+	notcreated//!< notcreated L'indice n'a pas de capteur associé.
 } EtatCapteur;
 
 
+/**
+ * Les données spécifiques d'une structure PEDEV.
+ */
 typedef struct
 {
-	EtatCapteur state; // identifiant logique du capteur.
-	int address; // l'adresse CAN du capteur
+	char name[NAME_SIZE]; ///< name Le nom du capteur.
+	EtatCapteur state; ///< state L'état du capteur.
+	int address; ///< address L'adresse CAN du capteur.
 } specific_t;
 
+/**
+ * Membre d'une liste chainée gérée par l'ios. Contient des
+ * des informations de gestion dans le membre specific.
+ */
 typedef struct  
 {
 	DEV_HDR header;
 	specific_t specific;
 } PEDEV;
 
+/**
+ * Structure représentant un message.
+ */
 typedef struct
 {
-	unsigned ID; // identifiant du message
-	struct timespec date; // date de reception
-	char msg[MAX_MSG_SIZE]; // contenu
+	unsigned id; ///< id identifiant du message
+	struct timespec date; ///< date date de reception
+	char msg[MAX_DATA_SIZE]; ///< msg contenu
 } Message;
 
 
@@ -48,15 +65,25 @@ typedef enum {
 	EALREADYOPENED, /** Le capteur est déjà ouvert */
 	ENOTOPENED, /** Le capteur n'est pas ouvert */
 	ECPTBUSY, /** Le capteur est en cours d'utilisation */
+	ECPTALREADYUSED, /** Le capteur est deja associe a un autre peripherique */
 	ENOAVAIL, /** Aucun message n'est disponible */
 	EINSTALLED, /** Le pilote est déjà installé */
 	ENINSTALLED, /** Le pilote n'est pas installé */
-	ETOOMUCHDEV, /** Nombre maximal de périphériques installés atteint */
+	ETOOMUCHDEV, /** Nombre de périphériques demandé superieur à ceux disponible */
 	EUNKNOW, /** Erreur inconnue */
 	ECANNOTADD /** Impossible d'ajouter un capteur */
+};
+
+/**
+ * Valeurs possibles de request pour ioctl.
+ */
+typedef enum {
+	SET_CPT_ADDRESS /** Changer l'adresse d'un capteur */
 };
 
 int pe_driverInstall(int dev_count);
 int pe_driverUninstall();
 int pe_deviceAdd(int i);
 int pe_deviceRemove(int i);
+
+void hardware_mockup(int address, char* data);
--- PEDriver/unittest.c
+++ PEDriver/unittest.c
@@ -5,8 +5,10 @@
  */
 
 #include "driver.h"
+#include <vxWorks.h>
 #include <errnoLib.h>
 #include <stdio.h>
+#include <string.h>
 
 /* Écrit un message d'erreur sur la sortie d'erreur */
 #define ERR_MSG(x) printf("Test %d failed. errno : %d\n",(x), errnoGet());
@@ -22,17 +24,17 @@
 int test_1()
 {
 	int success = 0;
-	if(pe_driverInstall(10))
+	if(pe_driverInstall(10) >= 0)
 	{
 		success++;
+		iosDrvShow();
+		getchar();
+		// Call this to clean the system.
 	}
 	else
 	{
 		ERR_MSG(1);
 	}
-	iosDrvShow();
-	getchar();
-	// Call this to clean the system.
 	pe_driverUninstall();
 	return success;
 }
@@ -63,9 +65,7 @@ int test_2()
 	{
 		ERR_MSG(2)
 	}
-	
-	printf("67");
-	
+		
 	// Call this to clean the system.
 	pe_driverUninstall();
 	return success;
@@ -126,27 +126,51 @@ int test_5()
 {
 	int success = 0;
 	int fd = 0;
+	Message buffer;
 
 	pe_driverInstall(10);
 	
 	// Ouvertude d'un capteur.
-	fd = creat("/dev/capteur0", O_RDONLY);
-	if( ! fd)
+	fd = open("/dev/capteur5", O_RDONLY, 0);	
+	ioctl(fd, SET_CPT_ADDRESS, 42);
+	hardware_mockup(42, "test 5\0");
+
+
+	// l'attente est obligatoire sinon le message n'est pas encore traité.
+	sleep(1);
+	read(fd,(char*)&(buffer), sizeof(buffer));
+	
+	if(strncmp(buffer.msg, "test 5\0", 7) != 0)
 	{
-		printf("Erreur d'ouverture du periphérique : test_5\n");
-		return success;
+		printf("Erreur a la lecture du message\n");
 	}
 	
-	// Tentative de retrait du driver : cela doit être un échec.
-	if( pe_driverUninstall() == ERROR )
+	if( fd != ERROR )
 	{
-		if(errnoGet() == ECPTBUSY)
-			success++;
+		// Tentative de retrait du driver : cela doit être un échec.
+		if(pe_driverUninstall() == ERROR)
+		{
+			if(errnoGet() == ECPTBUSY)
+				success++;
+			else
+				printf("Pas le bon code errno : test_5\n");
+		}
 		else
-			printf("Pas le bon code errno : test_5\n");
+		{
+			printf("driver enleve avec succes : Echec\n");
+		}
+		
+		close(fd);
 	}
+	else
+	{
+		printf("Erreur a l'ouverture du capteur\n");
+	}	
+	pe_driverUninstall();
+		
 	return success;
 }
+
 /**
  * Test 6 – Ajout d’un périphérique
  * Description
@@ -180,6 +204,8 @@ int test_6()
 		ERR_MSG(6);
 	}
 	
+	pe_driverUninstall();
+	
 	return success;
 }
 /**
@@ -188,7 +214,7 @@ int test_6()
  * Utilisation de la fonction iosDevDelete, avec des paramètres valides.
  * Resultat attendu
  * Il ne doit plus être possible d’ouvrir le périphérique : un appel à open sur ce périphérique doit
- * échouer (il doit un nombre négatif), et errno doit être positionné à ENEXIST.
+ * échouer (il doit un nombre négatif), et errno doit être positionné à ENOENT.
  **/
 int test_7()
 {
@@ -196,6 +222,7 @@ int test_7()
 	int fd = 0;
 
 	// on n'installe pas de device par défaut.
+	
 	pe_driverInstall(0);
 	
 	// On utilise la procédure qui encapsule iosDevAdd.
@@ -203,16 +230,15 @@ int test_7()
 	{
 		if(pe_deviceRemove(0) == OK)
 		{
-			if(creat("/dev/capteur0", O_RDONLY) < 0)
+			if(open("/dev/capteur0", O_RDONLY, 0) == ERROR)
 			{
-				if(errnoGet() == ENEXIST)
+				if(errnoGet() == ENOENT)
 				{
 					success++;
 				}
 				else
 				{
 					ERR_MSG(7);
-					printf("Pas bon code d'erreur : 7\n");
 				}
 			}
 			else
@@ -225,19 +251,38 @@ int test_7()
 			printf("L'ajout a échoué : 7\n");
 		}
 	}
+	else
+	{
+		printf("Erreur a l'ajout du peripherique\n");
+	}
+
+	pe_driverUninstall();
 	
 	return success;
 }
 /**
  * Test 8 – Ajout d’un périphérique alors que 15 périphériques ont été ajoutés.
  * Description
- * Utilisation de la fonction iosDevAdd, 16 fois, avec des paramètres valides.
- * Resultat attendu
- * Le 16e appel à iosDevAdd doit provoquer une erreur, et renvoyer ERROR.
+ * Utilisation de la fonction iosDevAdd sur un périphérique déjà ajouté.
+ * L'appel à iosDevAdd doit provoquer une erreur, et renvoyer ERROR.
  **/
 int test_8()
 {
-	return 0;	
+	int success = 0;
+	
+	if(pe_driverInstall(15) == ERROR)
+	{
+		return success;
+	}
+		
+	if(pe_deviceAdd(0) == ERROR)
+	{
+		success++;
+	}
+	
+	pe_driverUninstall();
+	
+	return success;
 }
 /**
  * Test 9 – Ouverture d’un capteur
@@ -249,7 +294,32 @@ int test_8()
  **/
 int test_9()
 {
-	return 0;	
+	int success = 0;
+	int fd = 0;
+
+	pe_driverInstall(10);
+	
+	// Ouvertude d'un capteur.
+	fd = open("/dev/capteur0", O_RDONLY, 0);
+	if( fd == ERROR )
+	{
+		printf("Erreur d'ouverture du peripherique\n");
+	}
+	else
+	{
+		success++;
+		if(close(fd))
+		{
+			printf("Erreur pendant la fermeture du capteur\n");
+		}
+	}
+	
+	if(pe_driverUninstall())
+	{
+		printf("Erreur de désinstallation du driver\n");
+	}
+	
+	return success;
 }
 /**
  * Test 10 – Ouverture d’un capteur déjà ouvert
@@ -261,7 +331,43 @@ int test_9()
  **/
 int test_10()
 {
-	return 0;	
+	int success = 0;
+	int fd1 = 0;
+	int fd2 = 0;
+	
+	pe_driverInstall(10);
+	
+	// Ouvertude d'un capteur.
+	fd1 = open("/dev/capteur0", O_RDONLY, 0);
+	if( fd1 == ERROR )
+	{
+		printf("Erreur d'ouverture du peripherique\n");
+		if( pe_driverUninstall() == ERROR )
+		{
+			printf("Erreur de désinstallation du driver\n");
+		}
+		return success;
+	}
+	
+	fd2 = open("/dev/capteur0", O_RDONLY, 0);
+	if( fd2 == ERROR )
+	{
+		success++;
+	}
+	
+	close(fd2);
+	
+	if(close(fd1))
+	{
+		printf("Erreur pendant la fermeture du capteur\n");
+	}
+	
+	if(pe_driverUninstall())
+	{
+		printf("Erreur a l'enlevement du driver\n");
+	}
+	
+	return success;	
 }
 /**
  * Test 11 – Ouverture d’un capteur avec des paramètres invalides
@@ -274,7 +380,33 @@ int test_10()
  **/
 int test_11()
 {
-	return 0;	
+	int success = 0;
+	int fd = 0;
+	
+	pe_driverInstall(1);
+	
+	// Ouvertude d'un capteur.
+	fd = open("/dev/capteur0", O_RDWR, 0);
+	if( fd == ERROR )
+	{
+		if(errnoGet() == EARG)
+		{
+			success++;
+		}
+		else
+		{
+			printf("Mauvais errno");
+		}
+	}
+	
+	close(fd);
+	
+	if(pe_driverUninstall())
+	{
+		printf("Erreur a l'enlevement du driver\n");
+	}
+	
+	return success;
 }
 /**
  * Test 12 – Fermeture d’un capteur
@@ -286,37 +418,181 @@ int test_11()
  **/
 int test_12()
 {
-	return 0;	
+	int success = 0;
+	int fd = 0;
+	
+	pe_driverInstall(10);
+	
+	// Ouvertude d'un capteur.
+	fd = open("/dev/capteur0", O_RDONLY, 0);
+	if( fd != ERROR )
+	{
+		if(close(fd) == ERROR)
+		{
+			printf("Erreur a la fermeture du capteur\n");
+		}
+		else
+		{
+			success++;
+		}
+	}
+	else
+	{
+		printf("Erreur a l'ouverture du capteur\n");
+	}
+
+	if(pe_driverUninstall())
+	{
+		printf("Erreur a l'enlevement du driver\n");
+	}
+
+	return success;
 }
 /**
  * Test 13 – Lecture d’une valeur dans un capteur
  * Description
- * Utiliser read sur un capteur ouvert avec succès
+ * Utiliser read sur un capteur ouvert avec succès avant puis après avoir
+ * envoyé un message
  * Resultat attendu
- * La valeur de retour doit être un nombre positif, et doit être cohérente par rapport aux paramètre
+ * La valeur de retour doit être : 
+ * - ERROR avec errno sur ENOAVAIL
+ * - un nombre positif, et doit être cohérente par rapport aux paramètre
  * d’appel de read.
  **/
 int test_13()
 {
-	return 0;	
+	int success = 0;
+	int fd = 0;
+	
+	Message buffer;
+	char* data = "fake DATA :)\0";
+	static int id = -1;
+	struct timespec date;
+	pe_driverInstall(10);
+	
+	// Ouvertude d'un capteur.
+	fd = open("/dev/capteur2", O_RDONLY, 0);
+	if(fd != ERROR)
+	{
+		ioctl(fd, SET_CPT_ADDRESS, 42);
+		
+		if(read(fd,(char*)&(buffer), sizeof(buffer)) < 0)
+		{
+			if(errnoGet() == ENOAVAIL)
+			{
+				success++;
+			}
+			else
+			{
+				printf("Erreur : mauvais message d'erreur\n");
+			}
+		}
+		else
+		{
+			printf("Erreur : lecture avec succés sur un capteur vide\n");
+		}
+		
+		hardware_mockup(42, data);
+		id++;
+		clock_gettime(CLOCK_REALTIME, &(date));
+		// l'attente est obligatoire sinon le message n'est pas encore traité.
+		sleep(1);
+		if(read(fd,(char*)&(buffer), sizeof(buffer)) < 0)
+		{
+			printf("Erreur a la lecture %d\n", errnoGet());
+		}
+		else
+		{
+			if(date.tv_sec != buffer.date.tv_sec)
+			{
+				printf("Erreur : mauvaise date \n");
+			}
+			else
+			{			
+				if(strncmp(buffer.msg, data, MAX_DATA_SIZE) == 0)
+				{
+					success++;
+				}
+				else
+				{
+					//printf("Message:\n- id : %d\n- date : %ld:%ld\n- Message : %s\n", buffer.id, buffer.date.tv_sec, buffer.date.tv_nsec, buffer.msg);
+		
+					printf("Erreur : le message recus n'est pas le message envoye\n");
+					//printf("envoye : %s\n", data);
+					//printf("recus : %s\n", buffer.msg);
+					
+				}
+			}
+		}
+		
+		if(close(fd) == ERROR)
+		{
+			printf("Erreur a la fermeture du capteur\n");
+		}
+	}
+	else
+	{
+		printf("Erreur a l'ouverture du capteur\n");
+	}
+
+
+	if(pe_driverUninstall() == ERROR)
+	{
+		printf("Erreur a l'enlevement du driver\n");
+	}
+
+	return (success == 2) ? 1 : 0;
 }
 /**
  * Test 14 – Utilisation de read avec une taille de lecture invalide
  * Description
- * Utilisation de l’appel système read avec une descripteur de fichier valide, mais avec une taille
+ * Utilisation de l’appel système read avec un descripteur de fichier valide, mais avec une taille
  * de lecture négative.
  * Resultat attendu
- * L’appel doit échouer en renvoyant -1, et errno doit être positionné à EARG.
+ * L’appel doit échouer en renvoyant ERROR, et errno doit être positionné à EARG.
  **/
 int test_14()
 {
-	return 0;	
+	int success = 0;
+	int fd = 0;
+	Message buffer;
+	
+	pe_driverInstall(1);
+	
+	// Ouvertude d'un capteur.
+	fd = open("/dev/capteur0", O_RDONLY, 0);
+	if(fd != ERROR)
+	{
+		hardware_mockup(0, "fake DATA :)\0");
+
+		if(read(fd, (char*)&(buffer), -42) == ERROR)
+		{
+			success++;
+		}
+		
+		if(close(fd) == ERROR)
+		{
+			printf("Erreur a la fermeture du capteur\n");
+		}
+	}
+	else
+	{
+		printf("Erreur a l'ouverture du capteur\n");
+	}
+		
+
+	if(pe_driverUninstall() == ERROR)
+	{
+		printf("Erreur a l'enlevement du driver\n");
+	}
+	
+	return success;		
 }
 /**
  * Test 15 – Utilisation de ioctl avec des paramètres corrects
  * Description
  * Utilisation de ioctl avec des paramètres corrects : un descripteur de fichier valide, une valeur
- * pour request égale à la constante CHANGEMENT_CAPTEUR, et une valeur pour value inférieur ou
+ * pour request égale à la constante SET_CPT_ADDRESS, et une valeur pour value inférieur ou
  * égale à 255, correspondant bien à un capteur valide.
  * Resultat attendu
  * La valeur de retour doit être égale à OK, ou alors elle doit être égale à ERROR, mais alors errno
@@ -324,7 +600,41 @@ int test_14()
  **/
 int test_15()
 {
-	return 0;	
+	int success = 0;
+	int fd = 0;
+	
+	pe_driverInstall(10);
+	
+	// Ouvertude d'un capteur.
+	fd = open("/dev/capteur0", O_RDONLY, 0);
+	if(fd != ERROR)
+	{
+		if(ioctl(fd, SET_CPT_ADDRESS, 42) == OK)
+		{
+			success++;
+		}
+		else
+		{
+			printf("Erreur lors de l'appel a ioctl\n");
+		}
+		
+		if(close(fd) == ERROR)
+		{
+			printf("Erreur a la fermeture du capteur\n");
+		}
+	}
+	else
+	{
+		printf("Erreur a l'ouverture du capteur\n");
+	}
+
+
+	if(pe_driverUninstall() == ERROR)
+	{
+		printf("Erreur a l'enlevement du driver\n");
+	}
+	
+	return success;
 }
 /**
  * Test 16 – Utilisation de ioctl avec de mauvais arguments
@@ -338,7 +648,48 @@ int test_15()
  **/
 int test_16()
 {
-	return 0;	
+	int success = 0;
+	int fd = 0;
+	
+	pe_driverInstall(10);
+	
+	// Ouvertude d'un capteur.
+	fd = open("/dev/capteur0", O_RDONLY, 0);
+	if(fd != ERROR)
+	{
+		if(ioctl(fd, 42, 42) == ERROR)
+		{
+			if(errnoGet() == EARG)
+			{
+				success++;
+			}
+			else
+			{
+				printf("Erreur : Mauvais code d'erreur\n");
+			}
+		}
+		else
+		{
+			printf("Erreur lors de l'appel a ioctl\n");
+		}
+		
+		if(close(fd) == ERROR)
+		{
+			printf("Erreur a la fermeture du capteur\n");
+		}
+	}
+	else
+	{
+		printf("Erreur a l'ouverture du capteur\n");
+	}
+
+
+	if(pe_driverUninstall() == ERROR)
+	{
+		printf("Erreur a l'enlevement du driver\n");
+	}
+	
+	return success;
 }
 /**
  * Test 17 – Utilisation de ioctl pour associer le même capteur à deux descripteur
@@ -347,22 +698,114 @@ int test_16()
  * On utilise la fonction ioctl deux fois, avec des descripteurs de fichier différents, mais avec le
  * même numéro de capteur.
  * Resultat attendu
- * L’appel doit échouer, et renvoyer ERROR. errno doit alors être positionné à EARG.
+ * L’appel doit échouer, et renvoyer ERROR. errno doit alors être positionné à ECPTALREADYUSED.
  **/
 int test_17()
 {
-	return 0;	
+	int success = 0;
+	int fd0 = 0;
+	int fd1 = 0;
+	
+	pe_driverInstall(10);
+	
+	// Ouvertude d'un capteur.
+	fd0 = open("/dev/capteur0", O_RDONLY, 0);
+	fd1 = open("/dev/capteur1", O_RDONLY, 0);
+	if(fd0 != ERROR && fd1 != ERROR)
+	{
+		if(ioctl(fd0, SET_CPT_ADDRESS, 42) == OK)
+		{
+			if(ioctl(fd0, SET_CPT_ADDRESS, 42) == ERROR)
+			{
+				if(errnoGet() == ECPTALREADYUSED)
+				{
+					success++;
+				}
+				else
+				{
+					printf("Erreur : Mauvais code d'erreur\n");
+				}
+			}
+			else
+			{
+				printf("Erreur : l'appel a ioctl n'a pas echoue, il existe deux capteur pointant vers la même adresse CAN\n");
+			}
+		}
+		else
+		{
+			printf("Erreur lors de l'appel a ioctl sur le premier capteur\n");
+		}
+		
+		if(close(fd0) == ERROR || close(fd1) == ERROR)
+		{
+			printf("Erreur a la fermeture d'un des capteurs\n");
+		}
+	}
+	else
+	{
+		printf("Erreur a l'ouverture d'un des capteurs\n");
+	}
+
+
+	if(pe_driverUninstall() == ERROR)
+	{
+		printf("Erreur a l'enlevement du driver\n");
+	}
+	
+	return success;
 }
 /**
  * Test 18 – Utilisation de write
  * Description
  * Appel de write sur un capteur ouvert avec succès.
  * Resultat attendu
- * L’appel doit échouer, et errno doit être positionné à ENOTSUP.
+ * L’appel doit échouer, et errno doit être positionné à ENSUP.
  **/
 int test_18()
 {
-	return 0;	
+	int success = 0;
+	int fd = 0;
+	Message buffer;
+	
+	pe_driverInstall(1);
+	
+	// Ouvertude d'un capteur.
+	fd = open("/dev/capteur0", O_RDONLY, 0);
+	if(fd != ERROR)
+	{
+		if(write(fd,(char*)&(buffer), sizeof(Message)))
+		{
+			if(errnoGet() == ENOTSUP)
+			{
+				success++;
+			}
+			else
+			{
+				printf("Mauvais numero d'erreur\n");
+			}
+		}
+		else
+		{
+			printf("Erreur : lecture avec succes");
+		}
+		
+		if(close(fd) == ERROR)
+		{
+			printf("Erreur a la fermeture du capteur\n");
+		}
+	}
+	else
+	{
+		printf("Erreur a l'ouverture du capteur\n");
+	}
+
+
+	if(pe_driverUninstall() == ERROR)
+	{
+		printf("Erreur a l'enlevement du driver\n");
+	}
+	
+	return success;		
 }
 
 /**
@@ -420,31 +863,35 @@ int test_20()
 void run_suite()
 {
 	int count = 0;
-	count += test_1();
-	count += test_2();
-	count += test_3();
-	count += test_4();
-	count += test_5();
-	count += test_6();
-	count += test_7();
-	count += test_8();
-	count += test_9();
-	count += test_10();
-	count += test_11();
-	count += test_12();
-	count += test_13();
-	count += test_14();
-	count += test_15();
-	count += test_16();
-	count += test_17();
-	count += test_18();
-	count += test_19();
-	count += test_20();
+	test_1() ? count++ : printf("test_1 : ERROR\n");
+	test_2() ? count++ : printf("test_2 : ERROR\n");
+	test_3() ? count++ : printf("test_3 : ERROR\n");
+	test_4() ? count++ : printf("test_4 : ERROR\n");
+	test_5() ? count++ : printf("test_5 : ERROR\n");
+	test_6() ? count++ : printf("test_6 : ERROR\n");
+	test_7() ? count++ : printf("test_7 : ERROR\n");
+	test_8() ? count++ : printf("test_8 : ERROR\n");
+	test_9() ? count++ : printf("test_9 : ERROR\n");
+	test_10() ? count++ : printf("test_10 : ERROR\n");
+	test_11() ? count++ : printf("test_11 : ERROR\n");
+	test_12() ? count++ : printf("test_12 : ERROR\n");
+	test_13() ? count++ : printf("test_13 : ERROR\n");
+	test_14() ? count++ : printf("test_14 : ERROR\n");
+	test_15() ? count++ : printf("test_15 : ERROR\n");
+	test_16() ? count++ : printf("test_16 : ERROR\n");
+	test_17() ? count++ : printf("test_17 : ERROR\n");
+	test_18() ? count++ : printf("test_18 : ERROR\n");
+	test_19() ? count++ : printf("test_19 : ERROR\n");
+	test_20() ? count++ : printf("test_20 : ERROR\n");
+	
 
-	printf("%d/20 tests passed.\n", count);
 	if(count == 20)
 	{
 		printf("All tests passed successfully.\n");
+	}
+	else
+	{
+		printf("%d/20 tests passed.\n", count);
 	}
 }
 
